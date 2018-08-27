3d04857cf86a87fc017c389a619d074074c7f46f
Wesley Rocha <wesleyvsr@gmail.com>
2018-08-27 13:55:31 -0300

    adding 'wesleyrocha.pythonanywhare.com' to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k=ukc39!z^$c@!2x30jp%02dilt=ste!lp*)nbzw^b68%xv_!t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'wesleyrocha.pythonanywhere.com]
 
 
 # Application definition
